eb00464bb3f242ef9b763e7c5dc434b5cb74ddb4
tathithuyvn89 <61693986+tathithuyvn89@users.noreply.github.com>
2020-08-11 16:00:38 +0700

    fix loi hien thi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {FormsModule} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
+import {BlogsComponent} from "./blogs/blogs.component";
 
 const routes: Routes = [
   {path: 'auth',loadChildren: () => import('./auth/auth-routing.module').then(m => m.AuthRoutingModule)},
-
+  {path: '', component: BlogsComponent}
 ];
 
 @NgModule({
