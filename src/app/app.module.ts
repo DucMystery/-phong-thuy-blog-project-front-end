fdeff6791b602eea633b99d66096b571ea12d32e
tathithuyvn89 <61693986+tathithuyvn89@users.noreply.github.com>
2020-08-11 08:51:56 +0700

    Create register form and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogsComponent } from './blogs/blogs.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import {RegitrationComponent} from './auth/regitration/regitration.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {authInterceptorProviders} from "./_interceptor/auth.intorceptor";
+import {APP_BASE_HREF} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogsComponent
+    BlogsComponent,
+    AuthComponent,
+    LoginComponent,
+    RegitrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders,
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {TokenStorageService} from "../../services/tokenStorage.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form: any ={};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMess = '';
+  roles: string[] = [];
+
+  constructor(private authService: AuthService,
+              private tokenStorage: TokenStorageService
+              ) { }
+
+  ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getUser();
+    }
+  }
+  onSubmit() {
+    this.authService.login(this.form).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        console.log(data.accessToken);
+        this.tokenStorage.saveUser(data.email);
+        console.log(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getUser().roles;
+      },
+      err => {
+        this.errorMess =  err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  }
+
+  reloadPage() {
+    window.location.reload();
+  }
+}
