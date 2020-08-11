e332aac26fb53e54fa99029c346032b69d47867c
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-08-11 15:51:06 +0700

    kết nối API lấy về ds blogs

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {BlogService} from '../services/blog.service';
+import {Iblog} from '../interface/iblog';
 
 declare var $: any;
 
@@ -9,58 +10,62 @@ declare var $: any;
   styleUrls: ['./blogs.component.css']
 })
 export class BlogsComponent implements OnInit {
-  blogs=[];
+  blogs: any = [];
 
-  constructor(private blogService:BlogService) {
+  constructor(private blogService: BlogService) {
   }
 
   ngOnInit(): void {
-    $(function(){
-      var i = 0;
-      $('.ftco-animate').waypoint( function( direction ) {
-
-        if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-
-          i++;
-
-          $(this.element).addClass('item-animate');
-          setTimeout(function(){
-
-            $('body .ftco-animate.item-animate').each(function(k){
-              var el = $(this);
-              setTimeout( function () {
-                var effect = el.data('animate-effect');
-                if ( effect === 'fadeIn') {
-                  el.addClass('fadeIn ftco-animated');
-                } else if ( effect === 'fadeInLeft') {
-                  el.addClass('fadeInLeft ftco-animated');
-                } else if ( effect === 'fadeInRight') {
-                  el.addClass('fadeInRight ftco-animated');
-                } else {
-                  el.addClass('fadeInUp ftco-animated');
-                }
-                el.removeClass('item-animate');
-              },  k * 50, 'easeInOutExpo' );
-            });
-
-          }, 100);
-
-        }
-
-      } , { offset: '95%' } );
-      var loader = function() {
-        setTimeout(function() {
-          if($('#ftco-loader').length > 0) {
-            $('#ftco-loader').removeClass('show');
-          }
-        }, 1);
-      };
-      loader();
-    });
     this.getAllBlogs();
   }
 
-  getAllBlogs(){      //nếu dùng API thật thì cần observable
-    this.blogs=this.blogService.getAllBlogByTime();
+  getAllBlogs() {
+    this.blogService.getAllBlogByTime().subscribe((resp: Iblog) => {
+      this.blogs = resp;
+      $(function() {
+        var i = 0;
+        $('.ftco-animate').waypoint(function(direction) {
+
+          if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
+
+            i++;
+
+            $(this.element).addClass('item-animate');
+            setTimeout(function() {
+
+              $('body .ftco-animate.item-animate').each(function(k) {
+                var el = $(this);
+                setTimeout(function() {
+                  var effect = el.data('animate-effect');
+                  if (effect === 'fadeIn') {
+                    el.addClass('fadeIn ftco-animated');
+                  } else if (effect === 'fadeInLeft') {
+                    el.addClass('fadeInLeft ftco-animated');
+                  } else if (effect === 'fadeInRight') {
+                    el.addClass('fadeInRight ftco-animated');
+                  } else {
+                    el.addClass('fadeInUp ftco-animated');
+                  }
+                  el.removeClass('item-animate');
+                }, k * 50, 'easeInOutExpo');
+              });
+
+            }, 100);
+
+          }
+
+        }, {offset: '95%'});
+        var loader = function() {
+          setTimeout(function() {
+            if ($('#ftco-loader').length > 0) {
+              $('#ftco-loader').removeClass('show');
+            }
+          }, 1);
+        };
+        loader();
+      });
+      console.log('ok');
+    });
   }
+
 }
--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,39 +1,21 @@
 import { Injectable } from '@angular/core';
 import {Iblog} from '../interface/iblog';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  private blogs:Iblog[]=[
-    {id:1,
-      title:'this is title 1',
-      content:'this is content 1'
-    },
-    {id:2,
-      title:'this is title 2',
-      content:'this is content 2'
-    },
-    {id:3,
-      title:'this is title 3',
-      content:'this is content 3'
-    },
-    {id:1,
-      title:'this is title 4',
-      content:'this is content 4'
-    },
-    {id:5,
-      title:'this is title 5',
-      content:'this is content 5'
-    }
-  ]
+  private blogs:Iblog[]=[];
+  private URL='http://localhost:8080/api/blogs/'
 
 
-  constructor() {
+  constructor(private http:HttpClient) {
 
   }
-  getAllBlogByTime(){
-    return this.blogs;
+  getAllBlogByTime():Observable<Iblog>{
+    return this.http.get<Iblog>(this.URL+'list');
   }
 }
