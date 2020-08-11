642682696ee83e903aa1099e4d41637d671c5297
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-08-11 11:45:35 +0700

    thêm blogs module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {BlogsComponent} from './blogs/blogs.component';
+import {BlogDetailComponent} from './blogs/blog-detail/blog-detail.component';
+
 
 const routes: Routes = [
-  {path:'login',component:BlogsComponent},
+  {path: 'blogs',
+    loadChildren: () => import('../blogs/blgos.module').then(m => m.AdminModule),
+    canActivate: [AdminGuard],
+  },
+
 
 ];
 
