23c302187144af91695af6ffd2ea2c52623a1b4e
tathithuyvn89 <61693986+tathithuyvn89@users.noreply.github.com>
2020-08-16 23:50:03 +0700

    create upload file component

--- src/app/album/album-detail/album-detail.component.ts
+++ src/app/album/album-detail/album-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ImageService} from "../../services/albumimage/image.service";
 
 @Component({
   selector: 'app-album-detail',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-detail.component.css']
 })
 export class AlbumDetailComponent implements OnInit {
+  createAlbum : FormData;
+  imageList: any[];
+  rowIndexArray: any[];
 
-  constructor() { }
+  constructor(private service: ImageService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.service.imageDetailList.snapshotChanges().subscribe(
+      list => {
+        this.imageList = list.map(item => { return item.payload.val(); });
+        this.rowIndexArray =  Array.from(Array(Math.ceil((this.imageList.length+1) / 3)).keys());
+      }
+    );
   }
+  submit(){
+
+  }
+
 
 }
--- src/app/album/album.module.ts
+++ src/app/album/album.module.ts
@@ -1,21 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
 import { AlbumCreateComponent } from './album-create/album-create.component';
-
-
-
+import { AlbumComponent } from './album.component';
+import {RouterModule, Routes} from "@angular/router";
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "../../environments/environment";
+import {AngularFirestoreModule} from "@angular/fire/firestore";
+import {AngularFireDatabaseModule} from "@angular/fire/database";
+const routes: Routes = [
+  {path:"upload/image",component:AlbumCreateComponent  },
+  {path:"upload", component: AlbumDetailComponent}
+  ];
 
 @NgModule({
-      declarations: [ AlbumDetailComponent, AlbumCreateComponent],
+      declarations: [ AlbumDetailComponent, AlbumCreateComponent, AlbumComponent],
   exports: [
     AlbumDetailComponent,
     AlbumCreateComponent
   ],
-    imports: [
-        CommonModule,
-        FormsModule
-    ]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    RouterModule.forChild(routes)
+  ]
 })
 export class AlbumModule { }
