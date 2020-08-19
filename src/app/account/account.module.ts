6aec2c373c90589174edb7511f662b4628280de6
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-08-19 14:34:21 +0700

    fix loi edit account

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -6,9 +6,20 @@ import {AngularFireModule} from "@angular/fire";
 import {environment} from "../../environments/environment";
 import {AngularFirestoreModule} from "@angular/fire/firestore";
 import {AngularFireDatabaseModule} from "@angular/fire/database";
-import {RouterModule} from "@angular/router";
+import {RouterModule, Routes} from '@angular/router';
+import {BlogsComponent} from '../blogs/blogs.component';
+import {BlogListComponent} from '../blogs/blog-list/blog-list.component';
+import {BlogDetailComponent} from '../blogs/blog-detail/blog-detail.component';
+import {BlogAddComponent} from '../blogs/blog-add/blog-add.component';
+import {BlogEditComponent} from '../blogs/blog-edit/blog-edit.component';
+import {AuthGuard} from '../_interceptor/auth.guard';
+import {BlogUserComponent} from '../blogs/blog-user/blog-user.component';
+import {BlogErrorComponent} from '../blogs/blog-error/blog-error.component';
+import {BlogsCategoryComponent} from '../blogs/blogs-category/blogs-category.component';
 
-
+const routes: Routes = [
+  {path: 'edit',component:AccountEditComponent}
+];
 
 @NgModule({
   declarations: [AccountEditComponent],
@@ -19,7 +30,8 @@ import {RouterModule} from "@angular/router";
     CommonModule,
     FormsModule, AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
-    AngularFireDatabaseModule, ReactiveFormsModule, RouterModule
+    AngularFireDatabaseModule, ReactiveFormsModule, RouterModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AccountModule { }
